1fde7d9fe1e6c46dd4141ad9233dc4cef6ac59d5
Juliana Mokwa <julimokwa@gmail.com>
2021-03-01 17:56:58 +0000

    User model

---
+++ db/migrate/20210301160421_add_first_name_last_name_address_city_nickname_to_users.rb
@@ -0,0 +1,9 @@
+class AddFirstNameLastNameAddressCityNicknameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :nickname, :string
+  end
+end
