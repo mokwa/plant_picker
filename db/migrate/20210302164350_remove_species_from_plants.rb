4c599e8b755b6433a3e1564d3cd85b2180499c7d
Juliana Mokwa <julimokwa@gmail.com>
2021-03-04 12:16:10 +0000

    db reset

--- db/migrate/20210302164350_remove_species_from_plants.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSpeciesFromPlants < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :plants, :species_id
-  end
-end
--- db/migrate/20210302164636_drop_species.rb
+++
@@ -1,5 +0,0 @@
-class DropSpecies < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :species
-  end
-end
