6b4a1f41451af5bb82caad5d9e7be99e4ce4ed92
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-03-04 14:55:04 +0000

    chat working

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomsController < ApplicationController
 
   def show
+    @message = Message.new
     @chatroom = Chatroom.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :plants
-  resources :chatrooms, only: :show 
+  
+  resources :chatrooms, only: :show do
+  resources :messages, only: :create
+  end
+
 end
